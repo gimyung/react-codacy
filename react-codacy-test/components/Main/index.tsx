c10f891a6499ae703052f3b4bac1dd9d3f302113
choegimyeong_macbook <monster1172@gentlemonster.com>
2025-05-14 09:05:32 +0900

    feature : codacy test

---
+++ react-codacy-test/components/Main/index.tsx
@@ -0,0 +1,8 @@
+export default function Main() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-3xl font-bold">Welcome to the Main Component!</h1>
+      <p className="mt-4 text-lg">This is a simple example of a main component.</p>
+    </div>
+  )
+}
